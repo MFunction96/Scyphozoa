b00b1b37cf2c88aabb6db5138bdcfe8f929d57cc
MFunction <mfunction96@gmail.com>
2022-05-24 09:35:29 +0800

    Add the database version check

--- Scyphozoa/Program.cs
+++ Scyphozoa/Program.cs
@@ -11,9 +11,24 @@ builder.Configuration.AddUserSecrets<Program>();
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetSection("Scyphozoa").GetConnectionString("DefaultConnection");
+var getDbVersion = Version.TryParse(builder.Configuration.GetSection("Database")["MinimalVersion"], out var dbMinVersion) ;
+if (!getDbVersion)
+{
+    Console.Error.WriteLine("Unable to fetch the minimal version of database!");
+    Console.Error.WriteLine("Abort!");
+    Environment.Exit(1);
+}
+
+var dbVersion = ServerVersion.AutoDetect(connectionString);
+if (dbVersion.Version.CompareTo(dbMinVersion) < 0)
+{
+    Console.Error.WriteLine("Current database version is below the minimal version requirement!");
+    Console.Error.WriteLine("Abort!");
+    Environment.Exit(1);
+}
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options
-        .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        .UseMySql(connectionString, dbVersion));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
