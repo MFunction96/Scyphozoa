8e6cef93b792bc0487a53fc5edcc88c274054611
MFunction <mfunction96@gmail.com>
2022-06-20 17:50:18 +0800

    Add react router

--- Scyphozoa/ClientApp/src/App.tsx
+++ Scyphozoa/ClientApp/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import 'rsuite/dist/rsuite.min.css';
@@ -8,6 +9,11 @@ import WYSIWYGEditor from "./WYSIWYGEditor";
 function App() {
 	return (
 		<div className="App">
+			<h1>Welcome to React Router!</h1>
+			<Routes>
+				<Route path="/" element={<Home/>}/>
+				<Route path="about" element={<About/>}/>
+			</Routes>
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo"/>
 				<p>
@@ -25,6 +31,37 @@ function App() {
 			<Button appearance="primary"> Hello world </Button>
 			<WYSIWYGEditor/>
 		</div>
+	);
+}
+
+function Home() {
+	return (
+		<>
+			<main>
+				<h2>Welcome to the homepage!</h2>
+				<p>You can do this, I believe in you.</p>
+			</main>
+			<nav>
+				<Link to="/about">About</Link>
+			</nav>
+		</>
+	);
+}
+
+function About() {
+	return (
+		<>
+			<main>
+				<h2>Who are we?</h2>
+				<p>
+					That feels like an existential question, don't you
+					think?
+				</p>
+			</main>
+			<nav>
+				<Link to="/">Home</Link>
+			</nav>
+		</>
 	);
 }
 
