bae895deb0e0398df2a33b4ad769d8dd70b473d6
MFunction <mfunction96@gmail.com>
2022-06-29 15:24:12 +0800

    Add scss basic style Update package.json

--- Scyphozoa/ClientApp/src/App.tsx
+++ Scyphozoa/ClientApp/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
-import './App.css';
+import './App.scss';
 import 'rsuite/dist/rsuite.min.css';
 import Layout from "./Layout";
 import Home from './Home';
--- Scyphozoa/ClientApp/src/Layout.tsx
+++ Scyphozoa/ClientApp/src/Layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
+import {Container, Nav, Sidebar, Sidenav} from "rsuite";
+import {Dashboard} from "@rsuite/icons";
+import "./Layout.scss";
 
 export declare interface LayoutProps {
 	Title: string
@@ -8,12 +11,28 @@ export declare interface LayoutProps {
 export default class Layout extends React.Component<LayoutProps> {
 	render() {
 		return (
-			<>
-				<h1>
-					This is layout.
-				</h1>
-				<Outlet/>
-			</>
+			<div>
+				<Container>
+					<Sidebar>
+						<Sidenav>
+							<Sidenav.Header>
+								<h1>Sidenav Header</h1>
+							</Sidenav.Header>
+							<Sidenav.Body>
+								<Nav activeKey="1">
+									<Nav.Item eventKey="1" icon={<Dashboard />}>
+										Dashboard
+									</Nav.Item>
+								</Nav>
+							</Sidenav.Body>
+						</Sidenav>
+					</Sidebar>
+					<h1>
+						This is layout.
+					</h1>
+					<Outlet/>
+				</Container>
+			</div>
 		);
 	}
 }
